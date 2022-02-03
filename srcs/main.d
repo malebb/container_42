9029ae8a6846023243dcf083e124d590fa45ac6a
mlebrun <matthieulebrun55@gmail.com>
2022-02-03 14:54:14 +0100

    create size function vector

--- srcs/main.d
+++
@@ -1,8 +0,0 @@
-srcs/main.o: srcs/main.cpp srcs/containers/Vector.hpp \
-  srcs/containers/Map.hpp srcs/containers/Stack.hpp
-
-srcs/containers/Vector.hpp:
-
-srcs/containers/Map.hpp:
-
-srcs/containers/Stack.hpp:
