036e01dcf3acce72c04e6e482d71f22576c178dc
mlebrun <matthieulebrun55@gmail.com>
2022-02-08 12:15:45 +0100

    try create class inherited from vector iterator

--- incl/containers/Vector.hpp
+++ incl/containers/Vector.hpp
@@ -145,7 +145,7 @@ namespace ft
 				return (!(*this < rhs));
 			}
 
-		private :
+		protected :
 
 			pointer		_it;
 	};
@@ -160,22 +160,37 @@ namespace ft
 		return (rhs + n);
 	}
 
+	template <typename T>
+	class const_vector_iterator : public ft::vector_iterator<T>
+	{
+		const_vector_iterator(const T* ptr) : _it(ptr)
+		{
+			std::cout << "ok ca passe lol" << std::endl;
+		}
+	};
+
 	template <class T, class Alloc = std::allocator<T> >
 	class vector
 	{
 		public :
 			typedef T											value_type;
 			typedef Alloc										allocator_type;
+
 			typedef typename allocator_type::reference			reference;
 			typedef typename allocator_type::const_reference	const_reference;
+
 			typedef typename allocator_type::pointer			pointer;
 			typedef typename allocator_type::const_pointer		const_pointer;
+
+			typedef ft::vector_iterator<value_type>				iterator;
+			typedef ft::const_vector_iterator<const value_type>		const_iterator;
+
+			typedef ft::reverse_iterator<iterator>				reverse_iterator;
+			typedef ft::reverse_iterator<const_iterator>		const_reverse_iterator;
+
+			typedef std::ptrdiff_t								difference_type;
 			typedef size_t										size_type;
 
-			typedef vector_iterator<value_type>				iterator;
-			typedef vector_iterator<const value_type>		const_iterator;
-			typedef reverse_iterator<iterator>				reverse_iterator;
-			
 			vector() : _size(0), _capacity(0)
 			{
 			}
@@ -367,6 +382,7 @@ namespace ft
 				_alloc.construct(this->_array + this->_size, val);
 				this->_size++;
 			}
+
 		private :
 			allocator_type		_alloc;
 			pointer				_array;
--- srcs/main.cpp
+++ srcs/main.cpp
@@ -294,6 +294,137 @@ void	test_vector_iterator()
 	std::cout << "it3->nb2 = " << it3->nb2 << std::endl;
 
 }
+/*
+void	test_const_vector_iterator()
+{
+	ft::vector<std::string>::const_iterator		it;
+	ft::vector<std::string>::const_iterator		it2;
+	ft::vector<Nb>::const_iterator				it3;
+	ft::vector<Nb>							nbs;
+	Nb										nb;
+	ft::vector<std::string>					strs;
+
+	strs.push_back("one");
+	strs.push_back("two");
+	strs.push_back("three");
+	strs.push_back("four");
+	strs.push_back("five");
+
+	nbs.push_back(nb);
+
+	std::cout << std::endl;
+	std::cout << "------------ Test Iterator ------------"
+	<< std::endl;
+
+	std::cout << std::endl;
+	std::cout << ">> Test iterate" << std::endl;
+	std::cout << std::endl;
+
+	for (ft::vector<std::string>::iterator it = strs.begin();
+			it != strs.end(); it++)
+	{
+		std::cout << "strs = " << *it << std::endl;
+	}
+
+	std::cout << std::endl;
+	std::cout << ">> Test iterator operator" << std::endl;
+	std::cout << std::endl;
+
+	
+	std::cout << "strs is a vector of string" << std::endl;
+	std::cout << std::endl;
+	std::cout << "it = strs.begin()" << std::endl;
+	std::cout << std::endl;
+
+	it = strs.begin();
+
+	std::cout << "*it = " << *it << std::endl;
+
+	std::cout << "it++" << std::endl;
+	it++;
+	std::cout << "*it = " << *it << std::endl;
+
+	std::cout << "it--" << std::endl;
+	it--;
+	std::cout << "*it = " << *it << std::endl;
+
+	std::cout << "*++it = " << *++it << std::endl;
+
+	std::cout << "*--it = " << *--it << std::endl;
+
+	std::cout << "*it++" << std::endl;
+	*it++;
+	std::cout << "*it = " << *it << std::endl;
+
+	std::cout << "*it--" << std::endl;
+	*it--;
+	std::cout << "*it = " << *it << std::endl;
+
+	std::cout << "it += 2" << std::endl;
+	it += 2;
+	std::cout << "*it = " << *it << std::endl;
+
+	std::cout << "it -= 2" << std::endl;
+	it -= 2;
+	std::cout << "*it = " << *it << std::endl;
+	
+	std::cout << "it[3] = " << it[3] << std::endl;
+
+	std::cout << "it2 = it + 2 " << std::endl;
+	it2 = it + 2;
+	std::cout << "*it2 = " << *it2 << std::endl;
+
+	std::cout << "it2 = 3 + it " << std::endl;
+	it2 = 3 + it;
+	std::cout << "*it2 = " << *it2 << std::endl;
+
+	std::cout << "*++it = " << *++it << std::endl;
+	std::cout << "it2 = it - 1 " << std::endl;
+	it2 = it - 1;
+	std::cout << "*it2 = " << *it2 << std::endl;
+
+	std::cout << "it - it2 = " << it - it2 << std::endl;
+	it = strs.begin();
+	std::cout << "*it = \"hello\"" << std::endl;
+	*it = "hello";
+
+	std::cout << "*it = " << *it << std::endl;
+	std::cout << "*++it = " << *++it << std::endl;
+	std::cout << "*it2 = " << *it2 << std::endl;
+	std::cout << "it2 = it" << std::endl;
+	it2 = it;
+
+	std::cout << "*it2 = " << *it2 << std::endl;
+	std::cout << "it = strs.begin() and it2 = strs.begin() + 1" << std::endl;
+	it =  strs.begin();
+	it2 = strs.begin() + 1;
+
+	std::cout << "it == it2 : ";
+	check_comparison(it == it2);
+
+	std::cout << "it != it2 : ";
+	check_comparison(it != it2);
+
+	std::cout << "it < it2 : ";
+	check_comparison(it < it2);
+
+	std::cout << "it > it2 : ";
+	check_comparison(it > it2);
+
+	std::cout << "it <= it2 : ";
+	check_comparison(it <= it2);
+
+	std::cout << "it >= it2 : ";
+	check_comparison(it >= it2);
+
+	std::cout << "nbs is a vector of class Nb" << std::endl;
+	it3 = nbs.begin();
+	it3 = nbs.begin();
+	std::cout << "it3->nb = " << it3->nb << std::endl;
+	std::cout << "it3->nb2 = " << it3->nb2 << std::endl;
+
+}
+*/
 
 void	test_vector_capacity(void)
 {
@@ -490,5 +621,9 @@ int	main(void)
 
 //	test_vector_capacity();
 	test_vector_access();
+	ft::vector<int>					nbrs;
+	ft::vector<int>::const_iterator		it;
+
+	it = nbrs.begin();
 	return (0);
 }
