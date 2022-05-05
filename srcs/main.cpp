0feea2a9b6c80e69ea3626ae66246f7cadaf1632
= <=>
2022-05-05 15:08:12 +0200

    feat: add timer

--- srcs/main.cpp
+++ srcs/main.cpp
@@ -5,6 +5,7 @@
 #include <stack>
 #include <map>
 #include <list>
+#include <sys/time.h>
 
 class A
 {
@@ -19,16 +20,32 @@ class A
 	int		_nb;
 };
 
+long long int	get_time_ms()
+{
+	struct timeval		tv;
+
+	gettimeofday(&tv, NULL);
+	return (tv.tv_sec / 1000 + tv.tv_usec * 1000);
+}
+
+long long int get_prog_time(long long int first_time)
+{
+	return (get_time_ms() - first_time);
+}
+
 int	main(void)
 {
+	long long int		first_time;
+
 	std::cout << std::endl;
 	std::cout << "[------------ TEST FT CONTAINER ------------]" << std::endl;
 	std::cout << std::endl;
 
+	first_time = get_time_ms();
 	test_stack();
 	test_vector();
 	test_map();
-
+	std::cout << "execution terminated after " << get_prog_time(first_time) << "ms" << std::endl;
 //	ft::map<int, int>		nbrs;
 
 //	nbrs[15] = 15;
---
+++ srcs/stl/tests_map_capacity.d
@@ -0,0 +1,24 @@
+srcs/stl/tests_map_capacity.o: srcs/tests_map_capacity.cpp incl/main.hpp \
+  incl/vector.hpp incl/iterator.hpp incl/reverse_iterator.hpp \
+  incl/type_traits.hpp incl/compare.hpp incl/map.hpp incl/functional.hpp \
+  incl/utility.hpp incl/stack.hpp
+
+incl/main.hpp:
+
+incl/vector.hpp:
+
+incl/iterator.hpp:
+
+incl/reverse_iterator.hpp:
+
+incl/type_traits.hpp:
+
+incl/compare.hpp:
+
+incl/map.hpp:
+
+incl/functional.hpp:
+
+incl/utility.hpp:
+
+incl/stack.hpp:
